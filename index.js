9c8176b772e8c3b6e5bc2a316ae07941786f1dc4
Mark Nosal <mark.c.nosal@gmail.com>
2023-02-11 20:25:07 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+function reverseString(word) {
+  const reversedWord = word.split('').reverse().join('')
+  return reversedWord
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word)
+  return word === reversedWord
 }
 
 /* 
   Add your pseudocode here
+  reverse the word
+  function reverseWord(word)
+  return true if word === reversed word
+  if (word === reversed word) {return true}
+  return false if word !== reversed word
+  else {return false}
 */
 
 /*
   Add written explanation of your solution here
+  reverse the word
+  return true if word === reversed word
+  return false if word !== reversed word
 */
 
 // You can run `node index.js` to view these console logs
